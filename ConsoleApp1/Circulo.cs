15c68b6753a7807e3d974703165ebbcdee677580
marco <marco@MARCO>
2023-01-04 20:36:08 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Circulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Circulo
+    {
+        public double Radio { get; set; } = 5.2;
+        public Circulo()
+        {
+
+        }
+        public Circulo(double radio)
+        {
+            Radio = radio;
+        }
+        
+        public double CalcularArea()
+        {
+            return Radio * Radio * Math.PI;
+        }
+
+        public double CalcularArea(int radio)
+        {
+            Radio= radio;
+            return Radio * Radio * Math.PI;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using ConsoleApp1;
+
+Circulo circulo3 = new();
+
+Console.WriteLine(circulo3.Radio);
+
+var areaCirculo3 = circulo3.CalcularArea();
+
+Console.WriteLine(areaCirculo3);
+
+var nuevaAreaCirculo3 = circulo3.CalcularArea(2);
+
+Console.WriteLine(nuevaAreaCirculo3);
